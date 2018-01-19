15cd07340d284b99743d3cd48c361c384cbcd51e
rsiket <Ronyo0204>
2018-01-19 10:29:43 +0100

    first commit

---
+++ src/main/java/ventocom/at/test/Application.java
@@ -0,0 +1,12 @@
+package ventocom.at.test;
+
+/**
+ * @author Robert Siket on 1/19/2018
+ */
+public class Application
+{
+    public static void main(String[] args)
+    {
+        System.out.println("started");
+    }
+}
---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * @author Robert Siket on 1/19/2018
+ */
+public class ApplicationTest
+{
+    @Test
+    public void test(){
+
+    }
+}
